6a49c11fdcb738f15d4536ed16687b8c4a8a872b
Konrad Slaby <slaby.konrad@gmail.com>
2019-01-05 23:41:21 +0100

    Improved graphs

--- 4GL_zad3/4GL_zad3/FuzzyLogic.R
+++ 4GL_zad3/4GL_zad3/FuzzyLogic.R
@@ -1,24 +1,16 @@
 library(FuzzyNumbers)
+library(ggplot2)
 
 wiekMlody <- TrapezoidalFuzzyNumber(0, 0, 16, 25)
 wiekSredni <- TrapezoidalFuzzyNumber(18, 26, 55, 60)
 wiekStary <- TrapezoidalFuzzyNumber(55, 60, 100, 100)
 
-'
-plot(wiekMlody, xlim = c(0, 100))
-plot(wiekSredni, add = TRUE)
-plot(wiekStary, add = TRUE)
-'
-
-'
 R1 <- vector()
 for (n in c(0:60)) {
     value <- (1 / n) * evaluate(wiekMlody, n) * evaluate(wiekSredni, n)
     value <- replace(value, is.na(value), 0)
     R1 <- c(R1, value)
 }
-plot(R1)
-'
 
 R2 <- vector()
 for (n in c(0:60)) {
@@ -26,4 +18,13 @@ for (n in c(0:60)) {
     value <- replace(value, is.na(value), 0)
     R2 <- c(R2, value)
 }
-plot(R2)
+
+plot(wiekMlody, xlim = c(0, 100), ylim = c(0, 1), xlab = "Wiek", ylab = "Przynale¿noœæ", col = "red")
+plot(wiekSredni, col = "blue", add = TRUE)
+plot(wiekStary, col = "orange", add = TRUE)
+legend(90, 0.5, legend = c("wiekMlody", "wiekSredni", "wiekStary"),
+       col = c(" red ", " blue ", " orange "), lty = 1, cex = 0.8,
+       title = "Legenda", text.font = 4, bg = 'lightblue')
+
+plot(R1, col = "blue", xlab = "Wiek")
+plot(R2, col = "green", xlab = "Wiek")
