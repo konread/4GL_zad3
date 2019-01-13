c85ca2c6d319e0e5c6f635b8b8d5286450ae531d
Konrad Slaby <slaby.konrad@gmail.com>
2019-01-13 01:22:53 +0100

    fix for fl

--- 4GL_zad3/4GL_zad3/FuzzyLogic.R
+++ 4GL_zad3/4GL_zad3/FuzzyLogic.R
@@ -1,30 +1,70 @@
 library(FuzzyNumbers)
 library(ggplot2)
 
-wiekMlody <- TrapezoidalFuzzyNumber(0, 0, 16, 25)
-wiekSredni <- TrapezoidalFuzzyNumber(18, 26, 55, 60)
-wiekStary <- TrapezoidalFuzzyNumber(55, 60, 100, 100)
+scaleFunction <- function(x) {(x - min(x)) / (max(x) - min(x)) }
 
-R1 <- vector()
-for (n in c(0:60)) {
-    value <- (1 / n) * evaluate(wiekMlody, n) * evaluate(wiekSredni, n)
-    value <- replace(value, is.na(value), 0)
-    R1 <- c(R1, value)
+R1 <- function(przynaleznosc1, przynaleznosc2) {
+    R1 <- vector()
+    s <- seq(0, 1, by = 0.01)
+    for (i in s) {
+        value <- (1 / i) * evaluate(przynaleznosc1, i) * evaluate(przynaleznosc2, i)
+        value <- replace(value, is.na(value), 0)
+        value <- replace(value, is.infinite(value), 0)
+        R1 <- c(R1, value)
+    }
+
+    return (list("sum"=sum(R1)/sum(s),"array"=R1))
 }
 
-R2 <- vector()
-for (n in c(0:60)) {
-    value <- (evaluate(wiekMlody, n) * evaluate(wiekSredni, n)) / evaluate(wiekMlody, n)
-    value <- replace(value, is.na(value), 0)
-    R2 <- c(R2, value)
+R2 <- function(przynaleznosc1, przynaleznosc2) {
+    R2 <- vector()
+    s <- seq(0, 1, by = 0.01)
+    for (i in s) {
+        value <- (evaluate(przynaleznosc1, i) * evaluate(przynaleznosc2, i)) / evaluate(przynaleznosc1, i)
+        value <- replace(value, is.na(value), 0)
+        value <- replace(value, is.infinite(value), 0)
+        R2 <- c(R2, value)
+    }
+
+    return(list("sum" = sum(R2) / sum(s), "array" = R2))
 }
 
-plot(wiekMlody, xlim = c(0, 100), ylim = c(0, 1), xlab = "Wiek", ylab = "Przynale¿noœæ", col = "red")
+wiekList <- c(0, 0, 16, 40, 18, 26, 55, 65, 30, 60, 100, 100)
+wiekList <- scaleFunction(wiekList)
+
+wiekMlody <- TrapezoidalFuzzyNumber(wiekList[1], wiekList[2], wiekList[3], wiekList[4])
+wiekSredni <- TrapezoidalFuzzyNumber(wiekList[5], wiekList[6], wiekList[7], wiekList[8])
+wiekStary <- TrapezoidalFuzzyNumber(wiekList[9], wiekList[10], wiekList[11], wiekList[12])
+
+wzrostList <- c(0, 0, 120, 155, 110, 130, 165, 180, 145, 175, 200, 200)
+wzrostList <- scaleFunction(wzrostList)
+
+wzrostNiski <- TrapezoidalFuzzyNumber(wzrostList[1], wzrostList[2], wzrostList[3], wzrostList[4])
+wzrostSredni <- TrapezoidalFuzzyNumber(wzrostList[5], wzrostList[6], wzrostList[7], wzrostList[8])
+wzrostWysoki <- TrapezoidalFuzzyNumber(wzrostList[9], wzrostList[10], wzrostList[11], wzrostList[12])
+
+
+plot(wiekMlody, xlim = c(0, tail(wiekList, n = 1)), ylim = c(0, 1), xlab = "Wiek", ylab = "Przynale¿noœæ", col = "red")
 plot(wiekSredni, col = "blue", add = TRUE)
 plot(wiekStary, col = "orange", add = TRUE)
-legend(90, 0.5, legend = c("wiekMlody", "wiekSredni", "wiekStary"),
-       col = c(" red ", " blue ", " orange "), lty = 1, cex = 0.8,
-       title = "Legenda", text.font = 4, bg = 'lightblue')
+legend(0, 0.5, legend = c("wiekMlody", "wiekSredni", "wiekStary"),
+       col = c(" red ", " blue ", " orange "), lty = 1, cex = 0.6,
+       title = "Legenda", text.font = 4, bg = "lightblue")
 
-plot(R1, col = "blue", xlab = "Wiek")
-plot(R2, col = "green", xlab = "Wiek")
+plot(wzrostNiski, xlim = c(0, tail(wzrostList, n = 1)), ylim = c(0, 1), xlab = "Wzrost", ylab = "Przynale¿noœæ", col = "red")
+plot(wzrostSredni, col = "blue", add = TRUE)
+plot(wzrostWysoki, col = "orange", add = TRUE)
+legend(0, 0.5, legend = c("wzrostNiski", "wzrostSredni", "wzrostWysoki"),
+       col = c(" red ", " blue ", " orange "), lty = 1, cex = 0.6,
+       title = "Legenda", text.font = 4, bg = "lightblue")
+
+
+r1 <- R1(wiekMlody, wzrostNiski)
+print(r1$sum)
+s <- seq(0, 1, by = 0.01)
+plot(s, r1$array, xlab = "Step by [0.01]", ylab = "R1")
+
+r2 <- R2(wiekMlody, wzrostNiski)
+print(r2$sum)
+s <- seq(0, 1, by = 0.01)
+plot(s, r2$array, xlab = "Step by [0.01]", ylab = "R2")
